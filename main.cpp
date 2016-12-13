de5612da534b4c817bafba04bce7c46a2810f6a4
Dorina Cabac <dorinasfriends@yahoo.com>
2016-12-13 11:27:06 +0200

    menu joc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "test" << endl;
+int menu()
+    {
+        int choice;
+        cout<<"MENU";
+        cout<<"\n";
+        cout<<"START GAME  (PRESS 1)";
+        cout<<"\n";
+        cout<<"EXIT (PRESS 2)";
+
+        cin>>choice;
+        if(choice==1)
+            return 0;
+         else
+            return 1;
+    };
+
+int main()
+
+{
+
+    if(menu()!=1)
+    {
+        cout << "test" << endl;
+    }
+    return 0;
 }
